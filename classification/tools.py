8c7a11614d441b378019bd43e9d9b200b92701fa
Tomáš Mikula <mail@tomasmikula.cz>
2021-06-06 15:30:10 +0200

    Update

--- classification/tools.py
+++ classification/tools.py
@@ -1,5 +1,7 @@
 from sklearn.metrics import confusion_matrix as confusion
 from sklearn.utils import resample
+import pandas as pd
+import numpy as np
 
 
 def model_performance(model, train_X, train_y, test_X, test_y):
@@ -19,3 +21,16 @@ def confusion_matrix(train_X, train_y, test_X, test_y):
 
     print("Test")
     print(f"TN: {tn}, TP: {tp}, FN: {fn}, FP: {fp}")
+
+
+def polynomial_features(data, p):
+    new_columns = []
+    for i in range(p):
+        degree = i + 1
+        if degree > 1:
+            new_columns.extend([f"{column}^{degree}" for column in data.columns])
+        else:
+            new_columns.extend(data.columns)
+    return pd.DataFrame(
+        np.hstack(tuple((data ** (i + 1) for i in range(p)))), columns=new_columns
+    )
