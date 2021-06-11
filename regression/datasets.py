40eee9178d7ad24e8fa759f745320211240cc1a9
Tomáš Mikula <mail@tomasmikula.cz>
2021-06-11 09:54:36 +0200

    Update

--- regression/datasets.py
+++ regression/datasets.py
@@ -1,71 +1,91 @@
 import os
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-HAPPINESS_DIR = os.path.join("datasets", "happines")
 
+def load_tips():
+    # https://www.kaggle.com/jsphyg/tipping?select=tips.csv
 
-def load_happines():
-    # https://www.kaggle.com/unsdsn/world-happiness
+    df = pd.read_csv(os.path.join(os.path.join("datasets", "tips"), "tips.csv"))
 
-    df = pd.read_csv(os.path.join(HAPPINESS_DIR, "2015.csv"), index_col=0)
+    df["sex"] = df["sex"].replace({"Female": 1, "Male": 0})
+    df["smoker"] = df["smoker"].replace({"Yes": 1, "No": 0})
 
-    # to lowercase
-    df.columns = df.columns.str.lower()
+    df = pd.get_dummies(df)
 
-    target = df["happiness score"]
-    df.drop(
-        ["happiness rank", "happiness score", "standard error", "dystopia residual"],
-        inplace=True,
-        axis=1,
-    )
+    target = df["tip"]
+    df.drop("tip", axis=1, inplace=True)
 
     df["target"] = target
 
-    df.rename(
-        columns={
-            "economy (gdp per capita)": "economy",
-            "health (life expectancy)": "health",
-            "trust (government corruption)": "trust",
-        },
-        inplace=True,
-    )
+    return train_test_split(df, test_size=0.3, random_state=22)
 
-    df.index.name = "country"
 
-    # df.sort_values("target", inplace=True)
-    df_train = df.sample(frac=1, random_state=22)
+# HAPPINESS_DIR = os.path.join("datasets", "happines")
 
-    df = pd.read_csv(os.path.join(HAPPINESS_DIR, "2016.csv"), index_col=0)
 
-    # to lowercase
-    df.columns = df.columns.str.lower()
+# def load_happines():
+#     # https://www.kaggle.com/unsdsn/world-happiness
 
-    target = df["happiness score"]
-    df.drop(
-        [
-            "happiness rank",
-            "happiness score",
-            "upper confidence interval",
-            "lower confidence interval",
-            "dystopia residual",
-        ],
-        inplace=True,
-        axis=1,
-    )
+#     df = pd.read_csv(os.path.join(HAPPINESS_DIR, "2015.csv"), index_col=0)
 
-    df["target"] = target
+#     # to lowercase
+#     df.columns = df.columns.str.lower()
 
-    df.rename(
-        columns={
-            "economy (gdp per capita)": "economy",
-            "health (life expectancy)": "health",
-            "trust (government corruption)": "trust",
-        },
-        inplace=True,
-    )
+#     target = df["happiness score"]
+#     df.drop(
+#         ["happiness rank", "happiness score", "standard error", "dystopia residual"],
+#         inplace=True,
+#         axis=1,
+#     )
 
-    df.index.name = "country"
+#     df["target"] = target
 
-    df_test = df.sample(frac=1, random_state=22)
+#     df.rename(
+#         columns={
+#             "economy (gdp per capita)": "economy",
+#             "health (life expectancy)": "health",
+#             "trust (government corruption)": "trust",
+#         },
+#         inplace=True,
+#     )
 
-    return df_train, df_test
+#     df.index.name = "country"
+
+#     # df.sort_values("target", inplace=True)
+#     df_train = df.sample(frac=1, random_state=22)
+
+#     df = pd.read_csv(os.path.join(HAPPINESS_DIR, "2016.csv"), index_col=0)
+
+#     # to lowercase
+#     df.columns = df.columns.str.lower()
+
+#     target = df["happiness score"]
+#     df.drop(
+#         [
+#             "happiness rank",
+#             "happiness score",
+#             "upper confidence interval",
+#             "lower confidence interval",
+#             "dystopia residual",
+#         ],
+#         inplace=True,
+#         axis=1,
+#     )
+
+#     df["target"] = target
+
+#     df.rename(
+#         columns={
+#             "economy (gdp per capita)": "economy",
+#             "health (life expectancy)": "health",
+#             "trust (government corruption)": "trust",
+#         },
+#         inplace=True,
+#     )
+
+#     df.index.name = "country"
+
+#     df_test = df.sample(frac=1, random_state=22)
+
+#     return df_train, df_test
