8552c213c58704a0d6343b8d055c94a8da22fda6
Tomáš Mikula <mail@tomasmikula.cz>
2021-06-14 11:10:24 +0200

    Update

--- regression/tools.py
+++ regression/tools.py
@@ -3,6 +3,73 @@ from sklearn.utils import resample
 import pandas as pd
 import numpy as np
 
+import statsmodels.api as sm
+from sklearn.base import BaseEstimator, RegressorMixin
+
+
+class SMWrapper(BaseEstimator, RegressorMixin):
+    """A universal sklearn-style wrapper for statsmodels regressors"""
+
+    def __init__(
+        self, model_class, fit_intercept=True, ridge=False, lasso=False, alpha=0.0
+    ):
+        self.model_class = model_class
+        self.fit_intercept = fit_intercept
+        self.ridge = ridge
+        self.lasso = lasso
+        self.alpha = alpha
+
+    def fit(self, X, y):
+        # features = X.columns
+        # X = X.values
+        # y = y.values
+        if self.fit_intercept:
+            X = sm.add_constant(X)
+        self.model_ = self.model_class(y, X)
+
+        if not (self.ridge or self.lasso):
+            self.results_ = self.model_.fit()
+        elif self.ridge:
+            self.results_ = self.model_.fit(L1_wt=0, alpha=self.alpha)
+        elif self.lasso:
+            self.results_ = self.model_.fit(L1_wt=1, alpha=self.alpha)
+
+        return self
+
+    def predict(self, X):
+        if self.fit_intercept:
+            X = sm.add_constant(X)
+        return self.results_.predict(X)
+
+    def summary(self):
+        return self.results_.summary()
+
+
+class SMWrapperRegularized(BaseEstimator, RegressorMixin):
+    """A universal sklearn-style wrapper for statsmodels regressors"""
+
+    def __init__(self, model_class, fit_intercept=True, alpha=0.0):
+        self.model_class = model_class
+        self.fit_intercept = fit_intercept
+        self.alpha = 0.0
+
+    def fit(self, X, y):
+        if self.fit_intercept:
+            X = sm.add_constant(X)
+        self.model_ = self.model_class(y, X)
+
+        self.results_ = self.model_.fit_regularized(alpha=self.alpha)
+
+        return self
+
+    def predict(self, X):
+        if self.fit_intercept:
+            X = sm.add_constant(X)
+        return self.results_.predict(X)
+
+    def summary(self):
+        return self.results_.summary()
+
 
 def model_performance(model, train_X, train_y, test_X, test_y):
     print("R-squared:")
