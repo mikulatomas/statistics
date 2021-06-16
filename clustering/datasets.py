da841d8d366ac3f9a7127065947cf29f05c76813
Tomáš Mikula <mail@tomasmikula.cz>
2021-06-16 17:12:21 +0200

    Update

--- clustering/datasets.py
+++ clustering/datasets.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
 
-def load_dutch():
+def dutch_data():
+    """Prepares and loads diabetes data
+
+    More information about data: De Deyne, Simon, et al. "Exemplar by feature applicability matrices and other Dutch normative data for semantic concepts." Behavior research methods 40.4 (2008): 1030-1048.
+
+    """
+
     df = pd.read_csv("datasets/artifacts.csv", index_col=0)
     df.columns.name = "features"
 
