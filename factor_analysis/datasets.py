098adbcca6ef53eefb15656316436a29ea878602
Tomáš Mikula <mail@tomasmikula.cz>
2021-06-17 10:46:28 +0200

    Update

--- factor_analysis/datasets.py
+++ factor_analysis/datasets.py
@@ -1,28 +1,91 @@
 import pandas as pd
 import os
-from sklearn.utils import resample
-from sklearn.model_selection import train_test_split
-import numpy as np
+from sklearn.preprocessing import StandardScaler
 
-from sklearn.datasets import load_diabetes as diabetes
+LABELS = {
+    "EXT1": "I am the life of the party.",
+    "EXT2": "I don't talk a lot.",
+    "EXT3": "I feel comfortable around people.",
+    "EXT4": "I keep in the background.",
+    "EXT5": "I start conversations.",
+    "EXT6": "I have little to say.",
+    "EXT7": "I talk to a lot of different people at parties.",
+    "EXT8": "I don't like to draw attention to myself.",
+    "EXT9": "I don't mind being the center of attention.",
+    "EXT10": "I am quiet around strangers.",
+    "EST1": "I get stressed out easily.",
+    "EST2": "I am relaxed most of the time.",
+    "EST3": "I worry about things.",
+    "EST4": "I seldom feel blue.",
+    "EST5": "I am easily disturbed.",
+    "EST6": "I get upset easily.",
+    "EST7": "I change my mood a lot.",
+    "EST8": "I have frequent mood swings.",
+    "EST9": "I get irritated easily.",
+    "EST10": "I often feel blue.",
+    "AGR1": "I feel little concern for others.",
+    "AGR2": "I am interested in people.",
+    "AGR3": "I insult people.",
+    "AGR4": "I sympathize with others' feelings.",
+    "AGR5": "I am not interested in other people's problems.",
+    "AGR6": "I have a soft heart.",
+    "AGR7": "I am not really interested in others.",
+    "AGR8": "I take time out for others.",
+    "AGR9": "I feel others' emotions.",
+    "AGR10": "I make people feel at ease.",
+    "CSN1": "I am always prepared.",
+    "CSN2": "I leave my belongings around.",
+    "CSN3": "I pay attention to details.",
+    "CSN4": "I make a mess of things.",
+    "CSN5": "I get chores done right away.",
+    "CSN6": "I often forget to put things back in their proper place.",
+    "CSN7": "I like order.",
+    "CSN8": "I shirk my duties.",
+    "CSN9": "I follow a schedule.",
+    "CSN10": "I am exacting in my work.",
+    "OPN1": "I have a rich vocabulary.",
+    "OPN2": "I have difficulty understanding abstract ideas.",
+    "OPN3": "I have a vivid imagination.",
+    "OPN4": "I am not interested in abstract ideas.",
+    "OPN5": "I have excellent ideas.",
+    "OPN6": "I do not have a good imagination.",
+    "OPN7": "I am quick to understand things.",
+    "OPN8": "I use difficult words.",
+    "OPN9": "I spend time reflecting on things.",
+    "OPN10": "I am full of ideas.",
+}
 
 
-def load_diabetes():
-    dataset = diabetes(as_frame=True)
+def personality_data():
+    """Loads personality data.
 
-    df = dataset["data"]
+    It is sampled vesion, original version has 1 000 000 data points: https://www.kaggle.com/tunguz/big-five-personality-test
+    """
+    df = pd.read_csv(
+        os.path.join("datasets", "personality_test", "personality.csv.zip"), index_col=0
+    )
 
-    df["target"] = dataset["target"]
+    # drop unnecessary columns
+    df.drop(
+        [
+            "dateload",
+            "screenw",
+            "screenh",
+            "introelapse",
+            "testelapse",
+            "endelapse",
+            "IPC",
+            "country",
+            "lat_appx_lots_of_err",
+            "long_appx_lots_of_err",
+        ]
+        + [column for column in df.columns if "_E" in column],
+        axis=1,
+        inplace=True,
+    )
 
-    df.columns = df.columns.str.lower()
+    # standartize data
+    scaler = StandardScaler().fit(df)
+    train_X = pd.DataFrame(scaler.transform(df), columns=df.columns)
 
-    df_train, df_test = train_test_split(df, test_size=0.3, random_state=22)
-
-    return df, df_train, df_test
-
-
-def load_dutch():
-    df = pd.read_csv("datasets/artifacts.csv", index_col=0)
-    df.columns.name = "features"
-
-    return df
+    return train_X
